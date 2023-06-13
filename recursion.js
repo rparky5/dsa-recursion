06f5ba89d989697efabcc11d325e9ff6427070ff
Ryan Park <ryrypark@gmail.com>
2023-06-13 12:26:31 -0700

    find index

--- recursion.js
+++ recursion.js
@@ -22,26 +22,44 @@ function everyOther(str) {
 /** find: return boolean depending on if val exists in array or not. */
 
 function find(arr, val) {
-
+  if (arr.length === 0) return false;
+  return arr[0] === val || find(arr.slice(1), val);
 }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
 function isPalindrome(str) {
-
+  if (str.length <= 1) return true;
+  return (str[0] === str[str.length-1]) && isPalindrome(str.slice(1, -1))
 }
 
 /** revString: return a copy of a string, but in reverse. */
 
 function revString(str) {
-
+  if (str.length === 0) return "";
+  return str[str.length-1] + revString(str.slice(0, -1))
 }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
 function findIndex(arr, val) {
-
+  if (arr.length === 0) return -1;
+  if (arr[0] === val) return 0;
+  return findIndex(arr.slice(1), val) === -1 ? -1 : 1 + findIndex(arr.slice(1), val)
 }
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] === val) {
+    return i;
+  }
+}
+return -1;
+
+/**
+ *
+ *
+ *
+ * fi(["duck", "cat", "pony"])
+ */
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
